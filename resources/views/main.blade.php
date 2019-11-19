927ae212e9742488c8719bf9a91e6c93400f73dd
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-19 13:17:51 +0900

    ブートストラップ導入

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,21 +1,20 @@
+<!doctype html><!-- HTML5-->
+<html lang="ja">
+<head>
+  <!-- 以下の meta tags（charset と viewport）は必須です-->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap の CSS（CDN経由）の読み込み -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title>Hello, world!</title>
+</head>
 <body>
-  <div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-      <div class="top-right links">
-        @auth
-          <a href="{{ url('/home') }}">Home</a>
-        @else
-          <a href="{{ route('login') }}">Login</a>
+  <h1>Hello, world!</h1>
  
-          @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-          @endif
-        @endauth
-      </div>
-    @endif
- 
-    <div class="content">
-      <div class="title m-b-md">{{$str_1}} {{$str_2}}</div>
-    </div>
-  </div>
+  <!-- 関連 JavaScript の読み込み-->
+  <!-- jQuery, Popper.js, Bootstrap JS の順に読み込みます-->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
+</html>
