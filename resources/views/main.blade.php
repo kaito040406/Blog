ee3ed36505599b20ae785a0448d91371f8c3f0f2
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-19 17:23:21 +0900

    font-size決定

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,6 +7,7 @@
     <!-- Bootstrap の CSS（CDN経由）の読み込み -->
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
     <!-- <link href="{{asset("css/main_page.css")}}" rel="stylesheet"> -->
+    
     <title>main page</title>
     <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300 rel="stylesheet">
     <link href="css/_reset.scss" rel="stylesheet" type="text/css">
