305d09fbf889e9dba3a6abbc069b87747d14eec8
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-19 15:00:04 +0900

    スタイルシート読み込み

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,8 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap の CSS（CDN経由）の読み込み -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+    <!-- <link href="{{asset("css/main_page.css")}}" rel="stylesheet"> -->
     <title>main page</title>
+    <link href="css/main_page.css" rel="stylesheet" type="text/css">
   </head>
   <body>
     <div class = "box">
