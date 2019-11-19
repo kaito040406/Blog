7cd1b789860a1d89c77c7b1a8a959b92464655b9
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-19 14:19:48 +0900

    メインページのhtml作成

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,24 +1,29 @@
 <!doctype html><!-- HTML5-->
 <html lang="ja">
-<head>
-  <!-- 以下の meta tags（charset と viewport）は必須です-->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Bootstrap の CSS（CDN経由）の読み込み -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <title>main page</title>
-</head>
-<body>
-  <header>
-    <div class = "main_title">
-      タイトルだぜ!!!
+  <head>
+    <!-- 以下の meta tags（charset と viewport）は必須です-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap の CSS（CDN経由）の読み込み -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>main page</title>
+  </head>
+  <body>
+    <div class = "title_box">
+      <div class = "main_title">
+        Profile
+      </div>
     </div>
-  </header>
- 
-  <!-- 関連 JavaScript の読み込み-->
-  <!-- jQuery, Popper.js, Bootstrap JS の順に読み込みます-->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
+    <div class = "content_box">
+      <div class = "introduction_box">
+        Introduction
+      </div>
+      <div class = "skill_box">
+        Skill
+      </div>
+      <div class = "production">
+        Production
+      </div>
+    </div>
+  </body>
 </html>
