26bc38317928b4db1a219992e4474ca5e3f84df5
kaito hasegawa <kaitohasegawa000@gmail.com>
2019-11-23 20:56:28 +0900

    ヘッダー付与

--- resources/views/production.blade.php
+++ resources/views/production.blade.php
@@ -11,60 +11,74 @@
     <title>main page</title>
     <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300 rel="stylesheet">
     <link href="css/_reset.scss" rel="stylesheet" type="text/css">
+    <link href="css/main_page.scss" rel="stylesheet" type="text/css">
     <link href="css/production.scss" rel="stylesheet" type="text/css">
   </head>
   <body>
-    <body>
-      <div class = "production_box">
-        <div class = "title_production">
-            Production
+    <header>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark mt-3 mb-3">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav4" aria-controls="navbarNav4" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="/">Home</a>
+          <div class="collapse navbar-collapse">
+              <ul class="navbar-nav">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="#"></a>
+                  </li>
+              </ul>
+          </div>
+      </nav>  
+    </header>
+    <div class = "production_box">
+      <div class = "title_production">
+          Production
+      </div>
+      <div class = "content_box">
+        <div class = "app_box">
+          <div class = "app_box_text">
+            App
+          </div>
         </div>
-        <div class = "content_box">
-          <div class = "app_box">
-            <div class = "app_box_text">
-              App
+        <div class = "youtube_box">
+          <div class = "youtube_box_text">
+            YouTube
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class = "app_production_list">
+      <div class = "app_production_list_title">
+        ■ Application list
+      </div>
+      <div class = "app_productions">
+        <div class = "app_production">
+          <div class = "app_title">
+            <div class = "app_title_text">
+              メルカリコピー
+            </div>
+            <div class = "app_text">
+              プログラミングスクール TEACH::EXPEARTにてチーム開発を行なった時のものです。
+              <a href=" http://3.112.169.24/">
+                <br>詳細はこちら
+              <a>
             </div>
           </div>
-          <div class = "youtube_box">
-            <div class = "youtube_box_text">
-              YouTube
+        </div>
+        <div class = "app_production">
+          <div class = "app_title">
+            <div class = "app_title_text">
+              FX用自動売買ツール auto-mode
+            </div>
+            <div class = "app_text">
+              FX向けの自動トレードツールを作成しました。
+              OANDA apiを用いて、為替の情報を取得し、短期、中期、長期でのトレンドの情報を計算させて
+              相場情報を判断し、売買するツールです。でも講座を用いて、利益を得るための調整を行なっております。今後は、機械学習を用いて、精度を向上させていく予定です。
+              <a href="{{url('/images/auto-mode.zip')}}" download><br>ダウンロードはこちら</a>
             </div>
           </div>
         </div>
       </div>
-      <div class = "app_production_list">
-        <div class = "app_production_list_title">
-          ■ Application list
-        </div>
-        <div class = "app_productions">
-          <div class = "app_production">
-            <div class = "app_title">
-              <div class = "app_title_text">
-                メルカリコピー
-              </div>
-              <div class = "app_text">
-                プログラミングスクール TEACH::EXPEARTにてチーム開発を行なった時のものです。
-                <a href=" http://3.112.169.24/">
-                  <br>詳細はこちら
-                <a>
-              </div>
-            </div>
-          </div>
-          <div class = "app_production">
-            <div class = "app_title">
-              <div class = "app_title_text">
-                FX用自動売買ツール auto-mode
-              </div>
-              <div class = "app_text">
-                FX向けの自動トレードツールを作成しました。
-                OANDA apiを用いて、為替の情報を取得し、短期、中期、長期でのトレンドの情報を計算させて
-                相場情報を判断し、売買するツールです。でも講座を用いて、利益を得るための調整を行なっております。今後は、機械学習を用いて、精度を向上させていく予定です。
-                <a href="{{url('/images/auto-mode.zip')}}" download><br>ダウンロードはこちら</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </body>
+    </div>
   </body>
 </html>
